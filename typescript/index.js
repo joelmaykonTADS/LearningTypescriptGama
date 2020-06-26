753f881601ac051b80ca93df534817c7bf550994
joelmaykonTADS <joelmaykon94@gmail.com>
2020-06-26 09:47:24 -0300

    index

---
+++ typescript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 10;
+var num2 = 2;
+var result = sum(num1, num2);
+console.log(result);
---
+++ typescript/index.ts
@@ -0,0 +1,9 @@
+function sum(num1:number, num2:number) {
+    return num1 + num2;
+}
+
+const num1 = 10;
+const num2 = 2;
+
+const result = sum(num1, num2);
+console.log(result);
