843d09e0150dab293e1c2e479905152351a85df1
joelmaykonTADS <joelmaykon94@gmail.com>
2020-06-26 10:27:56 -0300

    modificadores de acesso

--- typescript/classes.js
+++ typescript/classes.js
@@ -1,4 +1,5 @@
 "use strict";
+//Public attributes
 var DateType = /** @class */ (function () {
     function DateType(day, month, year) {
         if (year === void 0) { year = 1970; }
@@ -9,7 +10,8 @@ var DateType = /** @class */ (function () {
     return DateType;
 }());
 var date = new DateType(1, 1);
-console.log(date.day, date.month, date.year);
+console.log(date.day + "/" + date.month + "/" + date.year);
+//Private attributes 
 var DateType2 = /** @class */ (function () {
     function DateType2(day, month, year) {
         if (year === void 0) { year = 1970; }
@@ -20,7 +22,42 @@ var DateType2 = /** @class */ (function () {
     DateType2.prototype.getDay = function () {
         return this.day;
     };
+    DateType2.prototype.getMonth = function () {
+        return this.month;
+    };
+    DateType2.prototype.getYear = function () {
+        return this.year;
+    };
     return DateType2;
 }());
 var date2 = new DateType2(11, 2);
-console.log(date2.getDay());
+console.log(date2.getDay() + "/" + date2.getMonth() + "/" + date2.getYear());
+var Car = /** @class */ (function () {
+    function Car(model, velocityMax) {
+        if (velocityMax === void 0) { velocityMax = 220; }
+        this.model = model;
+        this.velocityMax = velocityMax;
+        this.velocity = 0;
+    }
+    Car.prototype.alterVelocity = function (acc) {
+        if (this.velocity + acc <= this.velocityMax) {
+            this.velocity += acc;
+            console.log("Velocity actual " + this.velocity);
+        }
+        else {
+            console.log("Velocity max " + this.velocity);
+        }
+    };
+    Car.prototype.accelerate = function () {
+        this.alterVelocity(5);
+    };
+    return Car;
+}());
+var car = new Car('Prism', 30);
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
--- typescript/classes.ts
+++ typescript/classes.ts
@@ -1,5 +1,5 @@
 //Public attributes
-class DateType{
+class DateType {
     day: number;
     month: number;
     year: number;
@@ -20,19 +20,19 @@ class DateType2 {
     private day: number;
     private month: number;
     private year: number;
-    
+
     constructor(day: number, month: number, year: number = 1970) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
-    
+
     getDay() {
         return this.day;
     }
 
     getMonth() {
-        return this.year;
+        return this.month;
     }
 
     getYear() {
@@ -42,4 +42,32 @@ class DateType2 {
 }
 
 const date2 = new DateType2(11, 2);
-console.log(`${date2.getDay()}/${date2.getMonth}/${date2.getYear()});
+console.log(`${date2.getDay()}/${date2.getMonth()}/${date2.getYear()}`);
+
+class Car {
+    private velocity: number = 0;
+    constructor(
+        public model: string,
+        private velocityMax: number = 220) { }
+    private alterVelocity(acc: number) {
+        if (this.velocity + acc <= this.velocityMax) {
+            this.velocity += acc;
+            console.log(`Velocity actual ${this.velocity}`)
+        } else {
+            console.log(`Velocity max ${this.velocity}`)
+        }
+    }
+    accelerate() {
+        this.alterVelocity(5);
+    }   
+}
+
+const car = new Car('Prism', 30)
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+car.accelerate();
+
