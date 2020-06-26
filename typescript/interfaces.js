de2b7eecb1607030a12fe667f1755cad64cdeabc
joelmaykonTADS <joelmaykon94@gmail.com>
2020-06-26 09:47:14 -0300

    interfaces typescript

---
+++ typescript/interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+function getUPerson() {
+    return {
+        name: 'Joel Maykon',
+        mail: 'joel@mail.com',
+        address: 'Natal/RN - BR'
+    };
+}
+function setPerson(person) {
+}
---
+++ typescript/interfaces.ts
@@ -0,0 +1,15 @@
+interface Person {
+    name: string;
+    mail: string;
+    address: string;
+}
+function getUPerson(): Person {
+    return {
+        name: 'Joel Maykon',
+        mail: 'joel@mail.com',
+        address: 'Natal/RN - BR'
+    }
+}
+function setPerson(person:Person) {
+    
+}
